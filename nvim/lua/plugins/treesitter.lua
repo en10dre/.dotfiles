62a171e427af80e546153767d2018fb0d06c3683
sketchycommits <149786490+sketchycommits@users.noreply.github.com>
2024-09-15 02:23:12 +0800

    added html, css, and javascript

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
                 -- A list of parser names, or "all" (the listed parsers MUST always be installed)
                 ensure_installed = { "c", "lua", "vim", "vimdoc", "query",
                     "markdown", "markdown_inline", "norg", "gleam", "perl",
-                    "scala", "python", "latex" },
+                    "scala", "python", "latex", "html", "css", "javascript" },
 
                 -- Install parsers synchronously (only applied to
                 -- `ensure_installed`)
