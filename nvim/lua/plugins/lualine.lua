9889296e37c654540b4526e51c2967dfd707621e
sketchycommits <149786490+sketchycommits@users.noreply.github.com>
2024-09-16 15:53:08 +0800

    udjusted some indents

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -45,9 +45,9 @@ return {
                     component_separators = '',
                     section_separators = '',
                     theme = {
-                        -- We are going to use lualine_c an lualine_x as left and
-                        -- right section. Both are highlighted by c theme .  So we
-                        -- are just setting default looks o statusline
+                        -- We are going to use lualine_c an lualine_x as left
+                        -- and right section. Both are highlighted by c theme .
+                        -- So we are just setting default looks o statusline
                         normal = { c = { fg = colors.fg, bg = colors.bg } },
                         inactive = { c = { fg = colors.fg, bg = colors.bg } },
                     },
@@ -87,7 +87,7 @@ return {
                 function()
                     return '▊'
                 end,
-                color = { fg = colors.blue }, -- Sets highlighting of component
+                color = { fg = colors.blue },      -- Sets highlighting of component
                 padding = { left = 0, right = 1 }, -- We don't need space before this
             }
 
@@ -183,7 +183,7 @@ return {
 
             -- Add components to right sections
             ins_right {
-                'o:encoding', -- option component same as &encoding in viml
+                'o:encoding',       -- option component same as &encoding in viml
                 fmt = string.upper, -- I'm not sure why it's upper case either ;)
                 cond = conditions.hide_in_width,
                 color = { fg = colors.green, gui = 'bold' },
