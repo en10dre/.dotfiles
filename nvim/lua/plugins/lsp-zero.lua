7f8ffc330343377909ee55bf62007238a5e19e14
sketchycommits <149786490+sketchycommits@users.noreply.github.com>
2024-09-17 07:35:27 +0800

    Setup for ts_ls and jdtls

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -35,7 +35,8 @@ return {
                 servers = {
                     ['lua_ls'] = { 'lua' },
                     ['html'] = { 'html' },
-                    ['ts_ls'] = { 'json', 'javascript', 'typescript' }
+                    ['ts_ls'] = { 'json', 'javascript', 'typescript' },
+                    ['jdtls'] = { 'java' },
                 }
             })
 
@@ -46,6 +47,17 @@ return {
                     return
                 end
                 -- Enable features specific to the language server
+                local opts = { buffer = bufnr }
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
             end
 
             -- Extend LSP configuration with error handling
@@ -83,6 +95,10 @@ return {
                         require('lspconfig').marksman.setup({
                         })
                     end,
+                    jdtls = function()
+                        require('lspconfig').jdtls.setup({
+                        })
+                    end,
                 }
             })
 
@@ -96,7 +112,6 @@ return {
                     { name = 'nvim_lsp' },
                     { name = 'vsnip' },
                     { name = 'path' },
-                    { name = 'biome' },
                 },
                 snippet = {
                     expand = function(args)
